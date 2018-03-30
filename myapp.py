6b82f424e663658a80bbb09ecaeff0320aefd3fe
philacher95 <36249259+philacher95@users.noreply.github.com>
2018-03-30 14:49:58 -0700

    Add files via upload
    website project

---
+++ myapp.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/article")
+def article():
+    return render_template("article.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
